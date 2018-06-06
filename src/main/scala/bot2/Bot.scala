5f0525d5d9a53b1a772f4e1feff2e3ac3d56130e
n.platonova <n.platonova@tinkoff.ru>
2018-06-06 09:39:47 +0500

    add bot

--- src/main/scala/bot2/Bot.scala
+++ src/main/scala/bot2/Bot.scala
@@ -1,45 +1,19 @@
 package bot2
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import akka.stream.ActorMaterializer
-import play.api.libs.json.JsValue
+import info.mukel.telegrambot4s.api.declarative.Commands
+import info.mukel.telegrambot4s.api.{Polling, TelegramBot}
 
-import scala.io.StdIn
+object Bot extends TelegramBot with Polling with Commands {
 
-object Bot {
-  def main(args: Array[String]) {
+  def token = "TOKEN"
 
-    implicit val system = ActorSystem("my-system")
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
+  val parser = Parser
+  val manager = PollManager
 
-    val pm = PollManager
-    val parser = Parser
-
-    val route: Route =
-      path("hello") {
-        post {
-  /*        entity(as[JsValue]) { rq =>
-
-            val command = (rq \ "").asOpt[String].getOrElse("")
-            val cmd = parser.parseInput(command)
-*/
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-//          }
-        }
-      }
-
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+  onCommand("coin", "flip") {
+    implicit msg => reply({
+      val cmd = parser.parseInput(msg.text.getOrElse(""))
+      manager.execute(msg.from.flatMap(_.username).getOrElse(""), cmd)
+    })
   }
 }
